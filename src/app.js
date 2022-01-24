bff697e4855eed1a0a7857c82bf94e966eab81d0
pereiraalvaro14 <pereiraalvaro@gmail.com>
2022-01-24 19:59:05 +0000

    TheDomainNameGenerator finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dominio = [".es", ".net", ".com"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < dominio.length; l++)
+        console.log(pronoun[i] + adj[j] + noun[k] + dominio[l]);
+    }
+  }
+}
